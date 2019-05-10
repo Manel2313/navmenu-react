8105b862cfe40aa6f1e77d0c054a429df970716d
Manel2313 <djemal.manel@gmail.com>
2019-05-10 11:01:02 +0100

    Updates

---
+++ precache-manifest.93e5163ad18b53d6da455611f203c2a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b8d5576382707fd28053e0fb5f2411",
+    "url": "/navmenu-react/index.html"
+  },
+  {
+    "revision": "af13fe14d2a988f9ea05",
+    "url": "/navmenu-react/static/css/main.58533a05.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/navmenu-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "af13fe14d2a988f9ea05",
+    "url": "/navmenu-react/static/js/main.84a40a10.chunk.js"
+  },
+  {
+    "revision": "f1e17e3150a46be93579",
+    "url": "/navmenu-react/static/js/runtime~main.44d054aa.js"
+  }
+]);
